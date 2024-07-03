315d91c8516567b49e7d67feb75edcf34a219f32
Basak Tepe <basaktepe2020@gmail.com>
2024-07-03 15:48:27 +0300

    handled many-to-many relations

--- ProjectApp/models.py
+++ ProjectApp/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Projects(models.Model):
     projectID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
-    language = models.CharField(max_length=100)
-    repositories = models.ManyToManyField('Repository', blank=True)
-    trackers = models.ManyToManyField('Tracker', blank=True)
+    language = models.CharField(max_length=50)
+    repositories = models.ManyToManyField('Repository', related_name='projects', blank=True)
+    trackers = models.ManyToManyField('Tracker', related_name='projects', blank=True)
 
     def __str__(self):
         return self.name
@@ -16,18 +16,18 @@ class Repository(models.Model):
     repositoryID = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     URL = models.URLField()
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
     email = models.EmailField()
     token = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-    
+
 class Tracker(models.Model):
     trackerID = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     URL = models.URLField()
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
     email = models.EmailField()
     token = models.CharField(max_length=100)
 
--- ProjectApp/serializers.py
+++ ProjectApp/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from ProjectApp.models import Projects, Repository, Tracker
 
-class ProjectsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Projects
-        fields = ('projectID', 'name', 'slug', 'description', 'language', 'repositories', 'trackers')
-
-
 class RepositorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Repository
         fields = ('repositoryID', 'title', 'URL', 'type', 'email', 'token')
 
-
 class TrackerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tracker
         fields = ('trackerID', 'title', 'URL', 'type', 'email', 'token')
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    repositories = RepositorySerializer(many=True, read_only=True)
+    trackers = TrackerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Projects
+        fields = ('projectID', 'name', 'slug', 'description', 'language', 'repositories', 'trackers')
